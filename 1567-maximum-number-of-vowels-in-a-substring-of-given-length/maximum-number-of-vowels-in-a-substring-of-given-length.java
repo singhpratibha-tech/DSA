368cdebefd11df2af4c4dc59aafbcdb46fba89c2
Pratibha Singh <singhpratibha12473@gmail.com>
2026-08-21 14:58:47 +0530

    Time: 24 ms (11.43%) | Memory: 46.6 MB (34.96%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,37 +1,41 @@
 class Solution {
     public int maxVowels(String s, int k) {
+
         HashSet<Character> vowels = new HashSet<>(
             Arrays.asList('a', 'e', 'i', 'o', 'u')
-            );
-
-        HashSet<Character> window = new HashSet<>();
+        );
 
         int count = 0;
         int max = 0;
-        for(int i = 0 ; i < k; i++){
-            char ch = s.charAt(i);
-            window.add(ch);
-           if(vowels.contains(ch)){
-            count++;
-           }
-        }
-            
 
-           max = count;
-           for(int i = k ; i < s.length(); i++){
-            char ch1 = s.charAt(i-k);
-            char ch2 = s.charAt(i);
-            window.remove(ch1);
-            window.add(ch2);
-            if(vowels.contains(ch1)){
-                count--;
-            }
-            if(vowels.contains(ch2)){
+        // First window
+        for (int i = 0; i < k; i++) {
+            char ch = s.charAt(i);
+
+            if (vowels.contains(ch)) {
                 count++;
             }
+        }
+
+        max = count;
+
+        // Sliding window
+        for (int i = k; i < s.length(); i++) {
+
+            char ch1 = s.charAt(i - k); // leaving
+            char ch2 = s.charAt(i);     // entering
+
+            if (vowels.contains(ch1)) {
+                count--;
+            }
+
+            if (vowels.contains(ch2)) {
+                count++;
+            }
+
             max = Math.max(max, count);
-           }
-           
+        }
+
         return max;
     }
 }
